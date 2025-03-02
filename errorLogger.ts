37b1ea39920e8bc0c02ffb1df440319f6a0b715d
mrvigneshvt <mrvigneshvt@gmail.com>
2025-03-02 22:05:09 +0530

    .

---
+++ errorLogger.ts
@@ -0,0 +1,12 @@
+import fs from "fs";
+
+export function errorLogger(text: string, error: unknown) {
+  const date = new Date();
+  const localTime = date.toLocaleString("en-Us", { timeZone: "Asia/Kolkata" });
+
+  fs.appendFileSync(
+    "errorLogs.txt",
+    `ERROR -: ${localTime} :-  ${text} : ${error}` + "\n",
+  );
+  console.log(text, error);
+}
